3cff8f4c2f639fee154232e5cbce75dd5099ff67
Bharat1298 <BharatKhadka1298@gmail.com>
2023-01-23 19:49:48 -0500

    Tuned PIDs for Auton, need to resolve module state at end of path

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -135,8 +135,8 @@ public final class Constants {
         public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
         public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
     
-        public static final double kPXController = 1;
-        public static final double kPYController = 1;
+        public static final double kPXController = -.125;
+        public static final double kPYController = -.125;
         public static final double kPThetaController = 1;
     
         /* Constraint for the motion profilied robot angle controller */
--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -11,19 +11,17 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
-import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 
 public class exampleAuto extends SequentialCommandGroup {
     public exampleAuto(Swerve s_Swerve, LimelightSubsystem limelightSubsystem){
-        PathPlannerTrajectory exampleTrajectory = PathPlanner.loadPath("2MTest", new PathConstraints(1, 1.0));
+        PathPlannerTrajectory exampleTrajectory = PathPlanner.loadPath("BackAndForth", new PathConstraints(4, 3));
         PIDController theta = new PIDController(Constants.AutoConstants.kPThetaController, 0, 0);
         theta.enableContinuousInput(-Math.PI, Math.PI);
 
@@ -38,11 +36,11 @@ public class exampleAuto extends SequentialCommandGroup {
         theta,
         s_Swerve::setModuleStates, 
         false,
-        s_Swerve);
+        s_Swerve); /*.andThen(s_Swerve.setModuleStates({0,0,0}))*/
 
         addCommands(
             new InstantCommand(() -> s_Swerve.resetOdometry(exampleTrajectory.getInitialHolonomicPose())),
-            new FollowPathWithEvents(path, exampleTrajectory.getMarkers(), eventMap)
+            new FollowPathWithEvents(path, exampleTrajectory.getMarkers(), eventMap)/*.andThen(s_Swerve.setModuleStates(null), s_Swerve)*/
         );
     }
 
