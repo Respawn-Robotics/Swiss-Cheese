d725781bd8992648e649036174af871795769f66
RespawnRobotics325 <respawnrobotics@gmail.com>
2023-02-27 19:54:20 -0500

    changed acquire and score values to make the collection motor go right way

--- src/main/java/frc/robot/commands/operator/OperatorCommands.java
+++ src/main/java/frc/robot/commands/operator/OperatorCommands.java
@@ -26,35 +26,35 @@ public class OperatorCommands {
     }
     
     public Command acquireConeFromFloor() {
-        return new Acquire(ArmConstants.ACQUIRE_FROM_FLOOR, WristConstants.ACQUIRE_FROM_FLOOR, true, armSubsystem, wristSubsystem, collectionSubsystem);
-    }
-
-    public Command acquireCubeFromFloor() {
         return new Acquire(ArmConstants.ACQUIRE_FROM_FLOOR, WristConstants.ACQUIRE_FROM_FLOOR, false, armSubsystem, wristSubsystem, collectionSubsystem);
     }
 
-    public Command acquireConeFromDoS() {
-        return new Acquire(ArmConstants.ACQUIRE_FROM_DOS, WristConstants.ACQUIRE_FROM_DOS, true, armSubsystem, wristSubsystem, collectionSubsystem);
+    public Command acquireCubeFromFloor() {
+        return new Acquire(ArmConstants.ACQUIRE_FROM_FLOOR, WristConstants.ACQUIRE_FROM_FLOOR, true, armSubsystem, wristSubsystem, collectionSubsystem);
     }
 
-    public Command acquireCubeFromDoS() {
+    public Command acquireConeFromDoS() {
         return new Acquire(ArmConstants.ACQUIRE_FROM_DOS, WristConstants.ACQUIRE_FROM_DOS, false, armSubsystem, wristSubsystem, collectionSubsystem);
     }
 
+    public Command acquireCubeFromDoS() {
+        return new Acquire(ArmConstants.ACQUIRE_FROM_DOS, WristConstants.ACQUIRE_FROM_DOS, true, armSubsystem, wristSubsystem, collectionSubsystem);
+    }
+
     public Command scoreInHighCone() {
-        return new Score(ArmConstants.SCORE_IN_HIGH_CONE, WristConstants.SCORE_IN_HIGH_CONE, true, armSubsystem, wristSubsystem, collectionSubsystem);
+        return new Score(ArmConstants.SCORE_IN_HIGH_CONE, WristConstants.SCORE_IN_HIGH_CONE, false, armSubsystem, wristSubsystem, collectionSubsystem);
     }
 
     public Command scoreInHighCube() {
-        return new Score(ArmConstants.SCORE_IN_HIGH_CUBE, WristConstants.SCORE_IN_HIGH_CUBE, false, armSubsystem, wristSubsystem, collectionSubsystem);
+        return new Score(ArmConstants.SCORE_IN_HIGH_CUBE, WristConstants.SCORE_IN_HIGH_CUBE, true, armSubsystem, wristSubsystem, collectionSubsystem);
     }
 
     public Command scoreInMidCone() {
-        return new Score(ArmConstants.SCORE_IN_MID_CONE, WristConstants.SCORE_IN_MID_CONE, true, armSubsystem, wristSubsystem, collectionSubsystem);
+        return new Score(ArmConstants.SCORE_IN_MID_CONE, WristConstants.SCORE_IN_MID_CONE, false, armSubsystem, wristSubsystem, collectionSubsystem);
     }
 
     public Command scoreInMidCube() {
-        return new Score(ArmConstants.SCORE_IN_MID_CUBE, WristConstants.SCORE_IN_MID_CUBE, false, armSubsystem, wristSubsystem, collectionSubsystem);
+        return new Score(ArmConstants.SCORE_IN_MID_CUBE, WristConstants.SCORE_IN_MID_CUBE, true, armSubsystem, wristSubsystem, collectionSubsystem);
     }
 
     public Command scoreInLowCone() {
